7769a137b7af9f7145757806f84f0b519f3a51f7
Edik <edikgylym@gmail.com>
2025-01-29 00:49:36 +0600

    vsfs

---
+++ Lab1/datatypes.py
@@ -0,0 +1,17 @@
+x=5
+print(type(x))
+
+x=20.444
+print(type(x))
+
+x=str("Edik")
+print (x)
+
+x = dict(name="John", age=36)
+print(x)
+
+x = range(6)
+print(x)
+
+x = list(("apple", "banana", "cherry"))
+print(x)
