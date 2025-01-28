590be54cb1ec09e77bb2f7cc8ce78ff3de158cf8
Edik <edikgylym@gmail.com>
2025-01-29 02:05:15 +0600

    dsaff

---
+++ Lab1/casting.py
@@ -0,0 +1,22 @@
+x=int(1)
+y=int(2.55)
+z=int("4")
+
+print (x)
+print (y)
+print (z)
+
+x= float (2)
+y= float (3.66)
+
+print (x)
+print (y)
+
+x= str("2s")
+y= str(3)
+z= str(6.77)
+
+print (x)
+print (y)
+print (z)
+
