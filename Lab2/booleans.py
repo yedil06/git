0fa3126cb21d3b546da727f4af57dac34c4836b8
Edik <edikgylym@gmail.com>
2025-02-02 08:02:57 +0600

    deeded

---
+++ Lab2/booleans.py
@@ -0,0 +1,34 @@
+print(10>9)
+print(10 == 9)
+print (10<9)
+
+a=22
+b=33
+
+if a>b :
+    print ("a is greater than b")
+else:
+    print ("a is not greater than b")
+
+print(bool("Hello"))
+print(bool(15))
+print(bool(0))
+
+def myFunction() :
+  return True
+
+print(myFunction())
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+x = 200
+print(isinstance(x , int))
+
+x= 30.44
+print(isinstance(x , float))
