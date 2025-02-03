fd553faceb43cf1bc9bf3b4532ce916d912cc583
Edik <edikgylym@gmail.com>
2025-02-04 05:16:25 +0600

    xsxsxssxaqqq

---
+++ Lab2/ifelse.py
@@ -0,0 +1,33 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+a=23
+b=77
+if a>b:
+  print ("a is greater than b")
+else:
+  print( "b is greater than a") 
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")     
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")  
