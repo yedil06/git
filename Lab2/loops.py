3af2efe3da53d83d7e973c6fabc9d5c5b98dc2d0
Edik <edikgylym@gmail.com>
2025-02-04 16:06:57 +0600

    nhjummm

---
+++ Lab2/loops.py
@@ -0,0 +1,31 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break  
+
+for i in range (2,7):
+  print (i)
+
+
+for i in range (2,7,2):
+  print (i)  
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+
+  adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
